b8e5f363ada611615811b1fadf6b1f870daeaf56
Joseph Derouin <joseph.derouin23@ycdsbk12.ca>
2021-03-03 15:59:46 +0000

    submit

--- problem1.py
+++ problem1.py
@@ -1,1 +1,20 @@
 
+#Enter Wins
+print("Enter your wins and losses for your team:")
+
+for i in range(6):
+    results = input("")
+
+
+
+if wins >= 5 :
+  print("You are placed in group 1.")
+
+elif wins >= 3 and win <= 4 :
+  print("You are placed in group 2.")
+
+elif wins >= 1 and win <= 2 :
+  print("You are placed in group 3.")
+
+elif wins <= 1 :
+  print("You were eliminated from the tournament.")
